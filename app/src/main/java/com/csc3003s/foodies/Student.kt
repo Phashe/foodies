ed4f4dc8065f11605ddf0069226d926f4f83c0f9
Phashe <82908348+Phashe@users.noreply.github.com>
2023-08-17 12:12:18 +0200

    Update Student.kt

--- app/src/main/java/com/csc3003s/foodies/Student.kt
+++ app/src/main/java/com/csc3003s/foodies/Student.kt
@@ -1,25 +1,45 @@
 package com.csc3003S.foodies
 
+/**
+ * Represents a student in the Foodies system, inheriting from the User class.
+ *
+ * @property userName The username of the student.
+ * @property userEmail The email address of the student.
+ * @property userPassword The password of the student.
+ * @property studentNumber The unique student number associated with the student.
+ * @property account The account information associated with the student.
+ */
 class Student(
     userName: String,
     userEmail: String,
     userPassword: String,
-    _studentNumber: String,
-    _account: String
+    private val _studentNumber: String,
+    private val _account: String
 ) : User(userName, userEmail, userPassword) {
 
+    /**
+     * The student's unique identification number.
+     */
     var studentNumber: String = _studentNumber
         get() = field
         set(value) {
             field = value
         }
 
+    /**
+     * The account information for the student.
+     */
     var account: String = _account
         get() = field
         set(value) {
             field = value
         }
 
+    /**
+     * Returns a string representation of the Student object.
+     *
+     * @return A string containing user information and student-specific details.
+     */
     override fun toString(): String {
         return "Student{User_Name='$userName', User_Email='$userEmail', StudentNumber='$studentNumber', Account=$account}"
     }
